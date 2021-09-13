e864241cd8d21b5d039e768a9a6a17c63d78c548
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-09-13 14:11:27 -0500

    html route to stats page

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -2,6 +2,9 @@ const path = require('path');
 const express = require('express');
 const router = express.Router();
 
+app.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"));
+  });
 
 
 module.exports = router;
