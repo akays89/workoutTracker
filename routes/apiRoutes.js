0798f9ff93569c7a99deae5a604ee9ecb0dd90ef
Amanda Kays <amanda@Amandas-MacBook-Air.local>
2021-09-15 14:33:06 -0500

    created route to get all workouts, works in insomnia

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,4 +1,25 @@
-const path = require('path');
 const router = require("express").Router();
+const Workout = require("../models").Workout;
+
+// get all workouts
+router.get("/api/workouts", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: { $sum: "$exercises.duration"}
+            }
+        }
+    ])
+    .then(data => {
+        res.json(data)
+    })
+    .catch (err => {
+        res.json(err);
+    });
+});
+
+
+
+
 
 module.exports = router;
